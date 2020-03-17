306dc06633137ab7cb6e64ac0364aa51b699fd8a
jimmycdunn <jimmy.dunn@kwhanalytics.com>
2020-03-17 15:58:02 -0700

    Updating remote with recent local postgres changes

--- start_docker_insurance_remote.sh
+++ start_docker_insurance_remote.sh
@@ -3,10 +3,17 @@ docker rm ins_develop
 MYPORT=$(grep "c.NotebookApp.port" ~/.jupyter/jupyter_notebook_config.py | tail -c 5)
 
 if [ "$1" == "--start-jupyter" ]; then
-    docker-compose run -p $MYPORT:8888 --name ins_develop insurance_env \
-        jupyter notebook --ip 0.0.0.0 --port $MYPORT --no-browser --allow-root --NotebookApp.token='kwh'
+    docker-compose run -d -p $MYPORT:8888 \
+	-v $SSH_AUTH_SOCK:/ssh-agent \
+	-e SSH_AUTH_SOCK=/ssh-agent \
+	--name ins_develop insurance_env && \
+        docker exec ins_develop jupyter notebook --ip 0.0.0.0 --port $MYPORT --no-browser --allow-root --NotebookApp.token='kwh'
 elif ["$1" == ""]; then
-    docker-compose run -p $MYPORT:8888 -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent --name ins_develop insurance_env bash
+    docker-compose run -p $MYPORT:8888 \
+	-v $SSH_AUTH_SOCK:/ssh-agent \
+	-e SSH_AUTH_SOCK=/ssh-agent \
+	--name ins_develop insurance_env bash && \
+	docker exec -it ins_develop bash
 else
     echo "Invalid arg. Please indicate --start-jupyter if you want to start Jupyter."\
         "Don't add an argument if you want to exec into bash on docker startup."
