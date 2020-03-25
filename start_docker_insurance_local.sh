9848fe2a222f5bacbfdf1079e567b640798b0003
jimmycdunn <jimmy.dunn@kwhanalytics.com>
2020-03-25 13:09:28 -0700

    Separated out local and remote docker-compose.yml files for clarity and modified start up shell scripts accordingly. Started to automate --create-dbs flag, will finalize once db persistance bug is solved

--- start_docker_insurance_local.sh
+++ start_docker_insurance_local.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 docker rm ins_develop
 
-NETWORKFLAG=$(grep "network_mode: host" ./docker-compose.yml |sed -e 's/[[:space:]]*//' |  head -c 1)
-JUPYTERPEMFLAG=$(grep "\- /etc/jupyter-kwha-cert:/etc/jupyter-kwha-cert" ./docker-compose.yml |sed -e 's/[[:space:]]*//' |  head -c 1)
-
-if [ "$NETWORKFLAG" != "#" ]; then
-    sed -i '' 's/network_mode/#&/' ./docker-compose.yml
-fi
-
-if [ "$JUPYTERPEMFLAG" != "#" ]; then
-    sed -i '' 's/\- \/etc\/jupyter-kwha-cert/#&/' ./docker-compose.yml
-fi
-
 if [ "$1" == "--start-jupyter" ]; then
-    docker-compose run -d --service-ports \
+    docker-compose -f local-docker-compose.yml run -d --service-ports \
 	-v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock \
         -e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" \
 	--name ins_develop insurance_env && \
         docker exec ins_develop jupyter notebook --ip 0.0.0.0 --port 8888 --no-browser --allow-root --NotebookApp.token='kwh'
+#elif [ "$1" == "--create-dbs" ]; then
+#    docker-compose -f local-docker-compose.yml run -d --service-ports \
+#        -v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock \
+#        -e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" \
+#        --name ins_develop insurance_env && \
+#	docker exec ins_develop bash \
+#        docker exec -w /root/insurance/marvin ins_develop python -m utils.restore_db && \
+#        docker exec -w /root/insurance ins_develop python ins_utils/download_koeppen_sql.py && \
+#	docker exec -w /root/insurance ins_develop bash gunzip koeppen.sql.gz && \
+#	docker exec -w /root/insurance ins_develop bash createdb koeppen && \
+#	docker exec -w /root/insurance ins_develop bash psql koeppen < koeppen.sql
 elif [ "$1" == "" ]; then
-    docker-compose run -d --service-ports \
+    docker-compose -f local-docker-compose.yml run -d --service-ports \
 	-v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock \
 	-e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" \
-	--name ins_develop insurance_env bash && \
+	--name ins_develop insurance_env && \
 	docker exec -it ins_develop bash
 else
     echo "Invalid arg. Please indicate --start-jupyter if you want to start Jupyter."\
--- start_docker_insurance_remote.sh
+++ start_docker_insurance_remote.sh
@@ -3,13 +3,13 @@ docker rm ins_develop
 MYPORT=$(grep "c.NotebookApp.port" ~/.jupyter/jupyter_notebook_config.py | tail -c 5)
 
 if [ "$1" == "--start-jupyter" ]; then
-    docker-compose run -d -p $MYPORT:8888 \
+    docker-compose -f remote-docker-compose.yml run -d -p $MYPORT:8888 \
 	-v $SSH_AUTH_SOCK:/ssh-agent \
 	-e SSH_AUTH_SOCK=/ssh-agent \
 	--name ins_develop insurance_env && \
         docker exec ins_develop jupyter notebook --ip 0.0.0.0 --port $MYPORT --no-browser --allow-root --NotebookApp.token='kwh'
 elif ["$1" == ""]; then
-    docker-compose run -d -p $MYPORT:8888 \
+    docker-compose -f remote-docker-compose.yml run -d -p $MYPORT:8888 \
 	-v $SSH_AUTH_SOCK:/ssh-agent \
 	-e SSH_AUTH_SOCK=/ssh-agent \
 	--name ins_develop insurance_env bash && \
