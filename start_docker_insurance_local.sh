cd985263394f2dae58304d0953fd11b2d6cc7194
jimmycdunn <jimmy.dunn@kwhanalytics.com>
2020-04-02 15:10:29 -0700

    Updated helper startup script to run Jupyter in background

--- start_docker_insurance_local.sh
+++ start_docker_insurance_local.sh
@@ -6,18 +6,8 @@ if [ "$1" == "--start-jupyter" ]; then
 	-v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock \
         -e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" \
 	--name ins_develop insurance_env && \
-        docker exec ins_develop jupyter notebook --ip 0.0.0.0 --port 8888 --no-browser --allow-root --NotebookApp.token='kwh'
-#elif [ "$1" == "--create-dbs" ]; then
-#    docker-compose -f local-docker-compose.yml run -d --service-ports \
-#        -v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock \
-#        -e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" \
-#        --name ins_develop insurance_env && \
-#	docker exec ins_develop bash \
-#        docker exec -w /root/insurance/marvin ins_develop python -m utils.restore_db && \
-#        docker exec -w /root/insurance ins_develop python ins_utils/download_koeppen_sql.py && \
-#	docker exec -w /root/insurance ins_develop bash gunzip koeppen.sql.gz && \
-#	docker exec -w /root/insurance ins_develop bash createdb koeppen && \
-#	docker exec -w /root/insurance ins_develop bash psql koeppen < koeppen.sql
+        docker exec -d ins_develop jupyter notebook --ip 0.0.0.0 --port 8888 --no-browser --allow-root --NotebookApp.token='kwh' && \
+	docker exec -it ins_develop bash
 elif [ "$1" == "" ]; then
     docker-compose -f local-docker-compose.yml run -d --service-ports \
 	-v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock \
